75a5927c4c07b327fa8e170772771ec9e0fd9cd3
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-29 14:58:29 -0400

    added empty methods for UserInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,55 @@
 package com.pluralsight;
-
-public class UserInterface {
-}
 // scanner is here. output to the screen the users input.
+public class UserInterface {
+    private Dealership dealership;
+
+    public UserInterface(Dealership dealership) {
+        this.dealership = dealership;
+    }
+
+    //methods
+    public void display(){
+
+    }
+
+    private void init(){
+
+    }
+
+    private void displayVehicles(){
+
+    }
+
+    public void processAllVehiclesRequest(){
+
+    }
+    
+    // empty methods
+    public void processGetByPriceRequest() {
+
+    }
+    public void processGetByMakeModelRequest() {
+
+    }
+    public void processGetByYearRequest() {
+
+    }
+    public void processGetByColorRequest() {
+
+    }
+    public void processGetByMileageRequest() {
+
+    }
+    public void processGetByVehicleTypeRequest() {
+
+    }
+    public void processGetAllVehiclesRequest() {
+
+    }
+    public void processAddVehicleRequest() {
+
+    }
+    public void processRemoveVehicleRequest() {
+
+    }
+}
