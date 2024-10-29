3b3811a0499346634991665562c3f363fd9b54a3
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-29 15:56:49 -0400

    completed helperDisplay method for UserInterface + added comments for this class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,19 +1,18 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-// scanner is here. output to the screen the users input.
 public class UserInterface {
     private Dealership dealership;
 
+    //empty constructor
     public UserInterface() {
     }
 
-    //methods
     public void display(){
-        init();
-        while(true){
+        init(); // the dealership data
+        while(true) {
             int option = helperDisplay();
             switch(option){
                 case 1:
@@ -43,31 +42,51 @@ public class UserInterface {
                 case 9:
                     processRemoveVehicleRequest();
                     break;
-
+                case 99:
+                    System.out.println("Thank you");
+                    return;
+                default:
+                    System.out.println("Invalid option, please try again.");
             }
         }
 
     }
 
     public int helperDisplay(){
-        System.out.println("Please select an option: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dealership Menu");
         System.out.println("1- Find vehicles within a price range ");
+        System.out.println("2- Find vehicles by make/model ");
+        System.out.println("3- Find vehicles by year ");
+        System.out.println("4- Find vehicles by color ");
+        System.out.println("5- Find vehicles by miles range ");
+        System.out.println("6- Find vehicles by type (car, truck, SUV, van) ");
+        System.out.println("7- All vehicles ");
+        System.out.println("8- Add a vehicle ");
+        System.out.println("9- Remove a vehicle ");
+        System.out.println("99-  Exit");
+
+        System.out.println("Please enter an option: ");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
+
 
     }
-
+    //method to Loading dealership object
     private void init(){
         DealershipFileManager manager = new DealershipFileManager();
         dealership = manager.getDealership();
 
     }
-
+    //display a list of vehicles
     private void displayVehicles(List<Vehicle> vehicle){
         for (Vehicle vehicle1 : vehicle) {
             System.out.println(vehicle1);
         }
 
     }
-
+    //show all the vehicles currently available 
     public void processAllVehiclesRequest(){
         displayVehicles(dealership.getAllVehicles());
 
